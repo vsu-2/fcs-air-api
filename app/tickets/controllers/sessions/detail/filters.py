9873de2fb0f187acdec85227cf5dab047688e2ce
GentleEnvy <komarov.sergei163@gmail.com>
2022-06-07 16:26:24 +0300

    optimize GET_TicketsSessionsDetailFiltersController._get_airports

--- app/tickets/controllers/sessions/detail/filters.py
+++ app/tickets/controllers/sessions/detail/filters.py
@@ -1,4 +1,4 @@
-from django.db.models import Count, F, Max, Min, Q
+from django.db.models import Count, Max, Min
 
 from app.base.controllers.base import BaseController
 from app.geo.models import *
@@ -21,8 +21,7 @@ class GET_TicketsSessionsDetailFiltersController(BaseController):
             'travel_time_range': self._get_travel_time_range(),
             'airlines': self._get_airlines(),
             'airports': self._get_airports(),
-            'offers': self._get_offers(),
-            'best_price': self._get_best_price()
+            'offers': self._get_offers()
         }
     
     def _get_transfers(self):
@@ -42,22 +41,18 @@ class GET_TicketsSessionsDetailFiltersController(BaseController):
         return Airline.objects.filter(
             abouts__segment__trip__ticket__in=self.ticket_ids
         ).distinct()
-    
+
     def _get_airports(self):
-        return Airport.objects.filter(
-            Q(segments_by_arrival__trip__ticket__in=self.ticket_ids) |
-            Q(segments_by_departure__trip__ticket__in=self.ticket_ids)
-        ).distinct()
-    
+        segments = Segment.objects.filter(
+            trip__ticket__in=self.ticket_ids
+        ).prefetch_related('arrival', 'departure')
+        airports = set()
+        for segment in segments:
+            airports.add(segment.arrival)
+            airports.add(segment.departure)
+        return airports
+
     def _get_offers(self):
-        return sorted(
-            Offer.objects.filter(
-                tickets_by_best_offer__in=self.ticket_ids
-            ).order_by('gate_id', 'price').distinct('gate_id'), key=lambda o: o.price
-        )
-    
-    def _get_best_price(self):
-        best_ticket = Ticket.objects.filter(id__in=self.ticket_ids).annotate(
-            best_price=F(f'best_offer__price')
-        ).order_by('best_price').first()
-        return None if best_ticket is None else best_ticket.best_price
+        return Offer.objects.filter(tickets_by_best_offer__in=self.ticket_ids).order_by(
+            'gate_id', 'price'
+        ).distinct('gate_id')
